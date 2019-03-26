f6d3efaa98a36bfa9ea498e4e740712c093d543c
y.soloshenko <Y.Soloshenko@gmail.com>
2019-03-26 08:36:07 +0200

    Data

---
+++ Persistance/DbService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Application;
+using DevChallenge.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance
+{
+    public class DbService : DbContext, IDBService
+    {
+        public DbSet<InstrumentPrice> InstrumentPrices { get ; set ; }
+
+        public void Save()
+        {
+            this.SaveChangesAsync();
+        }
+        public DbService() : base()
+        {
+            Database.SetInitializer(new DatabaseInitializer());
+        }
+
+        public void Save()
+        {
+            this.SaveChanges();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Configurations.Add(new CustomerConfiguration());
+            modelBuilder.Configurations.Add(new EmployeeConfiguration());
+            modelBuilder.Configurations.Add(new ProductConfiguration());
+            modelBuilder.Configurations.Add(new SaleConfiguration());
+        }
+    }
+}
